16c94a79ff4086ffcf9eb414cb58a322a1a87c98
amadoucodeur <amadoucodeur@gmail.com>
2024-03-15 18:03:43 +0000

    form,pricing#2

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-export default function ContactForm(){
+const ContactForm: React.FC = () => {
   return (
-    <div class="form-container">
-      <form class="form">
-        <div class="form-group">
-          <label for="email">Votre e-mail</label>
-          <input required="" name="email" id="email" type="text"/>
+    <div className="form-container">
+      <form className="form">
+        <div className="form-group">
+          <label htmlFor="email">Votre e-mail</label>
+          <input required name="email" id="email" type="email" />
         </div>
-        <div class="form-group">
-          <label for="textarea">Comment je peut vous aider?</label>
-          <textarea required="" cols="50" rows="10" id="textarea" name="textarea">          </textarea>
+        <div className="form-group">
+          <label htmlFor="textarea">Comment puis-je vous aider?</label>
+          <textarea required cols={50} rows={10} id="textarea" name="textarea"></textarea>
         </div>
-        <button type="submit" class="form-submit-btn">Submit</button>
+        <button type="submit" className="form-submit-btn">Envoyer</button>
       </form>
     </div>
-  )
+  );
 }
+
+export default ContactForm;
