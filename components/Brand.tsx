b887e598df96aa9d4cbc93f4386b3072662fe573
amadoucodeur <amadoucodeur@gmail.com>
2024-03-25 13:32:31 +0000

    qlq changements##

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import React from "react";
 
 export default function Brand() {
-  return <Image src="/logo.png" alt="logo" width={50} height={50} className="rounded-r-full fixed top-10 left-0 " />;
+  return <Image src="/logo.png" alt="logo" width={50} height={50} className="rounded-r-full w-[20px] md:w-[50px] fixed top-10 left-0 " />;
 }
